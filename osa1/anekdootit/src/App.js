6a411e7126513424915a1306a6df0e98af0db002
Krister Haverinen <krister@piilossa.fi>
2021-08-06 22:58:38 +0300

    Completed anekdootit

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length+1).join('0').split('').map(parseFloat))
+
+  const Header = ({title}) => (
+    <h1>{title}</h1>
+  )
+
+  const Button = ({handleClick, value}) => (
+    <button onClick={handleClick}>{value}</button>
+  )
+
+  const getRandom = () => {
+    let result = Math.floor(Math.random()*anecdotes.length)
+    if (result === selected) {
+      result = getRandom()
+    }
+
+    return result
+  }
+
+  const updateVotes = () => {
+    const newStatus = [...votes]
+    newStatus[selected] += 1
+    setVotes(newStatus)
+  }
+
+  const getMostVotes = () => {
+    const sortedVotes = [...votes].sort((a, b) => { return b - a })
+    return votes.indexOf(sortedVotes[0])
+  }
+
+  const Anecdote = ({index}) => (
+    <cite>"{anecdotes[index]}"</cite>
+  )
+
+  const Votes = ({index}) => (
+    <p>Has {votes[index]} votes</p>
+  )
+
+  return (
+    <div>
+      <Header title="Anecdote of the day" />
+      <Anecdote index={selected} />
+      <Votes index={selected} />
+
+      <Button handleClick={() => updateVotes()} value="Vote" />
+      <Button handleClick={() => setSelected(getRandom)} value="Next" />
+
+      <Header title="Anecdote with most votes" />
+      <Anecdote index={getMostVotes()} />
+      <Votes index={getMostVotes()} />
+    </div>
+  )
+}
+
+export default App
